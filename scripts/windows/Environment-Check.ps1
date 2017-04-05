bff45e5e226e68f8484c94b296fe1ce8da990c78
Xoke <xokesoru@gmail.com>
2017-04-05 14:22:36 -0700

    Minor fixes
    1. Corrected case, so it is the same over the entire script
    2. Fixed bug if you ran the script at 11:59, it would make one folder for today, then try to save it into a folder tomorrow
    3. Added checks for modules - if you try to run it on a non-dc it will error out.

--- scripts/windows/Environment-Check.ps1
+++ scripts/windows/Environment-Check.ps1
@@ -1,63 +1,88 @@
-# Environment-check.ps1
-# v0.1 - March 27, 2017
-# by Brian Johnson (https://7ms.us)
+################################################################################
 #
-# This script is designed to gather some helpful baseline information in an Active Directory environment.
-# Run this on a DC, and a subfolder of today's date will be created and all info will be dumped in there.
+# Environment-Check.ps1
 #
-
-
-#Create a directory with today's date
-New-Item -ItemType Directory -Path ".\$((Get-Date).ToString('yyyy-MM-dd'))"
-
-# Import the Active Directory module - we'll be making use of that
-import-module activedirectory
-
-# Import the GroupPolicy module - we'll need that too
-import-module grouppolicy
+# This script is designed to gather some helpful baseline information in an
+# Active Directory environment.
+# Run this on a DC, and a subfolder of today's date will be created and all
+# info will be dumped in there.
+# 
+################################################################################
+#
+# Versions
+#
+# No.  Date           Person
+# 0.1  Mar 27, 2017   Brian Johnson (https://7ms.us)
+# 0.2  Apr 05, 2017   Xoke (https://keybase.io/xoke)
+#
+#
+################################################################################
 
 # Set a variable of today using today's date
-$today = Get-Date -Format yyyy-MM-dd
+$Today = Get-Date -Format yyyy-MM-dd
+
+#Create a directory with today's date
+#New-Item -ItemType Directory -Path ".\$((Get-Date).ToString('yyyy-MM-dd'))"
+New-Item -ItemType Directory -Path ".\$Today"
+
+# Try to import the Active Directory module - we'll be making use of that
+If(!(Get-Module -Name ActiveDirectory))
+{
+	Import-Module ActiveDirectory
+}
+
+# Try to import the GroupPolicy module - we'll need that too
+If(!(Get-Module -Name GroupPolicy))
+{
+	Import-Module GroupPolicy
+}
+
+# Check if the modules loaded
+If(!(Get-Module -Name ActiveDirectory) -or !(Get-Module -Name GroupPolicy))
+{
+    Echo "Failed to load required modules"
+    Exit
+}
 
 # Check PowerShell version
-echo Lets check PS version
+Echo "Lets check PS version"
 $PSVersionTable.PSVersion
-$PSVersionTable.PSVersion > $today\powershell_version.txt
+$PSVersionTable.PSVersion > $Today\PowerShell_Version.txt
 
 # Search for users with passwords that don't expire and dump to text file
-search-adaccount -PasswordNeverExpires | FT Name,enabled -A > $today\Users_Passwords_Dont_Expire.txt
+Search-ADAccount -PasswordNeverExpires | FT Name,Enabled -A > $Today\Users_Passwords_Dont_Expire.txt
 
 # Find disabled users and dump to text file
-search-ADAccount -AccountDisabled | select samaccountname | sort samaccountname > $today\Users_Disabled.txt
+Search-ADAccount -AccountDisabled | Select SAMAccountname | sort SAMAccountname > $Today\Users_Disabled.txt
 
 # Find users who have never logged in, and dump to what?  A text file.
-search-adaccount -accountinactive -usersonly | select samaccountname | sort samaccountname > $today\Users_Never_Logged_In.txt
+Search-ADAccount -AccountInactive -UsersOnly | Select SAMAccountname | sort SAMAccountname > $Today\Users_Never_Logged_In.txt
 
 # Get members of Domain Admins group...and plop into text file
-get-adgroupmember "Domain Admins" | select name > $today\Groups_Domain_Admins.txt
+Get-ADGroupMember "Domain Admins" | Select Name > $Today\Groups_Domain_Admins.txt
 
 # Get members of Enterprise Admins group and export to .txt 
-get-adgroupmember "Enterprise Admins" | select name > $today\Groups_Enterprise_Admins.txt
+Get-ADGroupMember "Enterprise Admins" | Select Name > $Today\Groups_Enterprise_Admins.txt
 
 # Get members of Administrators group...in a text file 
-get-adgroupmember "Administrators" | select name > $today\Groups_Admins.txt
+Get-ADGroupMember "Administrators" | Select Name > $Today\Groups_Admins.txt
 
 # Get members of Schema Admins group....in a...text...file....
-get-adgroupmember "Schema Admins" | select name > $today\Groups_Schema_Admins.txt
+Get-ADGroupMember "Schema Admins" | Select Name > $Today\Groups_Schema_Admins.txt
 
 # Export default domain policy to HTML to...you know what?  I don't think I need to tell you what kind of file this dumps to
-Get-ADDefaultDomainPasswordPolicy > $today\GPO_Default_Domain.txt
+Get-ADDefaultDomainPasswordPolicy > $Today\GPO_Default_Domain.txt
 
 # Retrieving password policy
-Get-ADDefaultDomainPasswordPolicy > $today\GPO_Password_Policy.txt
+Get-ADDefaultDomainPasswordPolicy > $Today\GPO_Password_Policy.txt
 
 # Exporting all GPOs to html
-Get-GPOReport -All -ReportType HTML -Path $today\GPO_ALL.html
+Get-GPOReport -All -ReportType HTML -Path $Today\GPO_ALL.html
 
 # Get list of computers
-get-adcomputer -Properties * -Filter * | Export-csv -Path $today\Computers-ALL.csv
-get-ADComputer -Filter "Enabled -eq '$true'" | select -Expand name | sort-object > $today\Computers_Enabled_Shortlist.txt
-Get-ADComputer -Filter "Enabled -eq '$false'" > $today\Computers_Disabled.txt
+Get-ADComputer -Properties * -Filter * | Export-csv -Path $Today\Computers_ALL.csv
+Get-ADComputer -Filter "Enabled -eq '$true'" | Select -Expand Name | Sort-Object > $Today\Computers_Enabled_Shortlist.txt
+Get-ADComputer -Filter "Enabled -eq '$false'" > $Today\Computers_Disabled.txt
 
 # Under construction:
 # Get hotfixes from all servers
