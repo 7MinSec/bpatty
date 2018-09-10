723a25513f56b48b81fa4563bbb6027932e07736
Brian Johnson <brian@brianjohnson.tv>
2018-09-10 17:17:18 -0500

    In computers you have to spell stuff right
    Brian didn't spell stuff right in a script and YOU'LL NEVER GUESS WHAT HAPPENED NEXT.

--- scripts/windows/import_ad_users.ps1
+++ scripts/windows/import_ad_users.ps1
@@ -1,5 +1,6 @@
-# This script will import the list of users in "lusers.csv" found in the same folder.
-# This script assumes you've made an OU called "BULKUSERS" - adjust as necessary
-# This script also uses a domain called "i.got.worms" - you'll want to change that as well
-Import-Csv .\big-lusers.csv | foreach-object {
+# This script:
+# * Will import the list of users in "lusers.csv" found in the same folder.
+# * Assumes you've made an OU called "BULKUSERS" - adjust as necessary
+# * Uses a domain called "i.got.worms" - you'll want to change that as well
+Import-Csv .\lusers.csv | foreach-object {
 New-ADUser -Name $_.DisplayName -UserPrincipalName $_.UserPrincipalName -SamAccountName $_.Username -GivenName $_.GivenName -DisplayName $_.DisplayName -SurName $_.Surname -Path "OU=BULKUSERS,DC=i,DC=got,DC=worms" -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -force) -Enabled $True -PasswordNeverExpires $True -PassThru }
